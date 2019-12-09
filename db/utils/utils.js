bd8307e958b24a8c2b1d801a9de0128d2a4fdf31
Sam-Blundell <samcalblundell@googlemail.com>
2019-12-09 15:32:33 +0000

    initial table creation migrations done

---
+++ db/utils/utils.js
@@ -0,0 +1,14 @@
+exports.formatDates = list => {
+  // if (list.length === 0) return [];
+
+  // const newList = [...list];
+
+  // newList[0].time = newList[0].time.slice(0, -5).replace("T", " ");
+
+  // return newList;
+};
+
+exports.makeRefObj = list => {
+};
+
+exports.formatComments = (comments, articleRef) => { };
---
+++ spec/utils.spec.js
@@ -0,0 +1,35 @@
+const { expect } = require('chai');
+const {
+  formatDates,
+  makeRefObj,
+  formatComments,
+} = require('../db/utils/utils');
+
+// describe('formatDates', () => {
+//   it('should return an empty array when passed an empty array', () => {
+//     expect(formatDates([])).to.deep.equal([]);
+//   });
+//   it('should return an array containing a javascript date object when passed an array containing a UNIX timestamp', () => {
+//     const timeStamps = [{ time: '2019-12-09T14:16:24.541Z' }];
+//     const input = formatDates(timeStamps);
+//     const actual = [{ time: '2019-12-09 14:16:24' }];
+//     expect(input).to.deep.equal(actual);
+//   });
+//   it('should return an array containing javascript date objects when passed an array of objects containing UNIX timestamps', () => {
+//     const timeStamps = [
+//       {},
+//       {},
+//       {}
+//     ]
+//     const input = formatDates(timeStamps);
+//     const actual = [
+//       {},
+//       {},
+//       {}
+//     ]
+//   })
+// });
+
+describe('makeRefObj', () => { });
+
+describe('formatComments', () => { });
