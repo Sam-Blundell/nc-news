88dd56654e644985a657c279dfb4e297979c8a1c
Sam-Blundell <samcalblundell@googlemail.com>
2020-01-14 13:33:59 +0000

    added CORS handling

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const apiRouter = require('./routers/apiRouter');
-const { noRouteError, errorHandler } = require('./errorHandling')
-
+const { noRouteError, errorHandler } = require('./errorHandling');
+const cors = require('cors');
 
 const server = express();
+
+server.use(cors());
 
 server.use(express.json());
 
